61ff930962eb1a144df473990f2c9ad44fbaa0b6
joseph miguel <josephchimaobi099@gmail.com>
2025-03-19 06:44:38 +0000

    Added a send email feature for sending emails

---
+++ pages/Contact_Us.py
@@ -0,0 +1,6 @@
+import streamlit as sl
+sl.header('Contact Us')
+with sl.form(key='email_form'):
+  user_email = sl.text_input('Your email: ')
+  message = sl.text_area('Your message: ')
+  button = sl.form_submit_button('Submit')
